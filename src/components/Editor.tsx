204e7a789d9dcc48dee1e8cdbd8527b4a1ddf0f0
Harris Salimo <harrismailasalimo@gmail.com>
2024-07-21 19:48:16 +0300

    feat: create Editor component

---
+++ src/components/Editor.tsx
@@ -0,0 +1,20 @@
+import { ChangeEventHandler } from "react";
+
+export const Editor = ({
+  value,
+  onChange,
+}: {
+  value?: string;
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
+}) => {
+  return (
+    <div className="editor prose h-full max-w-full dark:prose-invert">
+      <h2>Editor</h2>
+      <textarea
+        className="h-5/6 w-full rounded-lg bg-gray-200 dark:bg-gray-800"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
