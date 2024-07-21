f88c09af1bb54037ee88a2be2bc75aff574db049
Harris Salimo <harrismailasalimo@gmail.com>
2024-07-21 19:48:42 +0300

    feat: create Preview component

---
+++ src/components/Previewer.tsx
@@ -0,0 +1,17 @@
+export const Previewer = ({
+  value,
+}: {
+  value?: {
+    __html: string & TrustedHTML & DocumentFragment & HTMLElement;
+  };
+}) => {
+  return (
+    <div className="preview prose h-full max-w-full dark:prose-invert">
+      <h2>Preview</h2>
+      <div
+        className="mt-3 h-5/6 w-full overflow-y-auto rounded-lg border border-gray-500 bg-gray-200 p-2 dark:bg-gray-800"
+        dangerouslySetInnerHTML={value}
+      />
+    </div>
+  );
+};
